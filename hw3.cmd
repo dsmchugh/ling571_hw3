46b5bd4945595a522b3848d047aef11f20f605db
David MS McHugh <dsmchugh@uw.edu>
2014-02-06 18:15:27 -0800

    add evalb results and condor files

---
+++ hw3.cmd
@@ -0,0 +1,10 @@
+Executable = hw3.py
+Universe   = vanilla
+arguments  = hw3/parses.train hw3/trained.pcfg hw3/sents.test hw3/parses.hyp
+getenv     = true
+output     = hw3/hw3.out
+error      = hw3.error
+Log        = hw3.log
+transfer_executable = false
+request_memory = 1024
+Queue
